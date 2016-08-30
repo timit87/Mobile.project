761e6f7fd4b564702ad28d43c55f04fcb8052f6e
timit87 <timit87@mail.ru>
2016-08-30 17:59:46 +0300

    Комменты в файле:-)

---
+++ www/js/script.js
@@ -0,0 +1,9 @@
+$('.b-content__top-star').on('click', function() {
+	$('.b-content__top-star').css('opacity', 0);
+	$('.b-content__top-star-click').css('opacity', 1);
+	$('.b-content__top-star-message').css('opacity', 1);
+	return false;
+});
+
+
+	
